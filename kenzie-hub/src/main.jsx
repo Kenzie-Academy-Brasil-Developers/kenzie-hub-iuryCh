44f544562d68fc9696bd03f0cda40165e3d3a97e
iurych <iury.vibe@gmail.com>
2023-02-13 09:38:25 -0300

    add strictmode

--- kenzie-hub/src/main.jsx
+++ kenzie-hub/src/main.jsx
@@ -6,11 +6,11 @@ import Globalstyles from "./styles/Globalstyles.js";
 import Reset from "./styles/Reset.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <BrowserRouter>
       <Reset />
       <Globalstyles />
       <App />
     </BrowserRouter>
-  /* </React.StrictMode> */
+  </React.StrictMode>
 );
